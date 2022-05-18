20082ee1b06d6df4543568c83b0a7bfd3dc9231d
Alper Efe Şahin <efe.mate@gmail.com>
2022-05-18 22:38:28 +0300

    fix: clickable appbar button

--- lib/presentation/pages/home/widgets/app_bar_custom_icon.dart
+++ lib/presentation/pages/home/widgets/app_bar_custom_icon.dart
@@ -1,5 +1,5 @@
 import 'package:e_commerce_ui/presentation/pages/home/constants/colors.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AppBarCustomIcon extends StatelessWidget {
   const AppBarCustomIcon(
@@ -17,18 +17,21 @@ class AppBarCustomIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: leftPadding, right: rightPadding, top: 20),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(30),
-            decoration: const BoxDecoration(
-              color: appBarIconBackgroundColor,
-              borderRadius: BorderRadius.all(Radius.circular(50)),
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(30),
+              decoration: const BoxDecoration(
+                color: appBarIconBackgroundColor,
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
             ),
-          ),
-          Icon(iconData, color: appBarIconButtonColor, size: 30)
-        ],
+            Icon(iconData, color: appBarIconButtonColor, size: 30)
+          ],
+        ),
       ),
     );
   }
